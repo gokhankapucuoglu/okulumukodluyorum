08d1ba2c8ef363b3ee54bea04a5dc676bfec8908
Ev - Monster PC - Erdal Gökhan KAPUCUOĞLU <gokhankapucuoglu@gmail.com>
2026-01-02 23:24:13 +0300

    PhpMyAdmin, Reverb Kurulumu

---
+++ database/migrations/2025_12_21_201223_create_schools_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('schools', function (Blueprint $table) {
+            $table->id();
+            $table->string('province');
+            $table->string('district');
+            $table->string('institution_code')->unique();
+            $table->string('name');
+            $table->string('type');
+            $table->string('education_type')->nullable();
+            $table->time('start_time')->nullable();
+            $table->time('end_time')->nullable();
+            $table->string('program')->nullable();
+            $table->string('address')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('email')->nullable();
+            $table->string('logo')->nullable();
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('schools');
+    }
+};
